e549a62e1152db15eacdee38643b3e0f98ba6c3c
RogeWood <a813712100@gmail.com>
2022-07-01 01:01:52 +0800

    adjest 462

--- Medium/462_Minimum_Moves_to_Equal_Array_Elements_II.cpp
+++ Medium/462_Minimum_Moves_to_Equal_Array_Elements_II.cpp
@@ -2,26 +2,11 @@ class Solution {
 public:
     int minMoves2(vector<int>& nums) {
         int len = nums.size();
-        sort(nums.begin(), nums.end());
+        sort(nums.begin(), nums.end());// 排序找中位數
 
-        int sum = 0;
-        if(len%2)
-        {
-            int target = nums[len/2];
-            for(int i = 0; i < len; i++)
-                sum += abs(nums[i] - target);
-        }
-        else
-        {
-            int t1 = nums[len/2], sum1 = 0;
-            int t2 = nums[len/2 - 1], sum2 = 0;
-            for(int i = 0; i < len; i++)
-            {
-                sum1 += abs(nums[i] - t1);
-                sum2 += abs(nums[i] - t2);
-            }
-            sum = (sum1 < sum2) ? sum1 : sum2;
-        }
+        int t = nums[len/2], sum = 0;
+        for(int i = 0; i < len; i++) // 將所有差距加起來
+            sum += abs(nums[i] - t);
         return sum;
     }
 };
