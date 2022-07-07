c38cfb233bc087b80c16bfc09335a5f995dfd8d3
RogeWood <a813712100@gmail.com>
2022-07-07 21:35:14 +0800

    add 97

--- Medium/97_Interleaving_String.cpp
+++ Medium/97_Interleaving_String.cpp
@@ -1,5 +1,4 @@
-// Author: Huhaua
-// Running time: 0 ms
+// Solution link: https://zxi.mytechroad.com/blog/dynamic-programming/leetcode-97-interleaving-string/
 class Solution
 {
 public:
@@ -8,14 +7,18 @@ public:
         int l1 = s1.length();
         int l2 = s2.length();
         int l3 = s3.length();
+
+        // 長度不合回傳 false
         if (l1 + l2 != l3)
             return false;
+
         // dp[i][j]: 是否 s3[0:i+j] 是由 s1[0:i] and s2[0:j] 組成
         vector<vector<int>> dp(l1 + 1, vector<int>(l2 + 1));
         dp[0][0] = true;
         for (int i = 0; i <= l1; ++i)
             for (int j = 0; j <= l2; ++j)
             {
+                // 如果 s1[i-1] 到 s3[i+j-1] 符合，改成 True
                 if (i > 0)
                     dp[i][j] |= dp[i - 1][j] && s1[i - 1] == s3[i + j - 1];
                 if (j > 0)
