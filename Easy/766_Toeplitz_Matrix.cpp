9f85eb3ff6a9aae039fb479887d2a7b6e88cddc3
RogeWood <a813712100@gmail.com>
2022-10-31 16:43:45 +0000

    add 766 commit

--- Easy/766_Toeplitz_Matrix.cpp
+++ Easy/766_Toeplitz_Matrix.cpp
@@ -4,7 +4,7 @@ public:
         int height = matrix.size(), width = matrix[0].size();
         for(int i = 1; i < height; i++)
             for(int j = 1; j < width; j++)
-                if(matrix[i][j] != matrix[i-1][j-1])
+                if(matrix[i][j] != matrix[i-1][j-1]) //檢查每一個 element 和左上是否相同
                     return false;
         return true;
     }
